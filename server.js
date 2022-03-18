a2a993b9b601201ecfdf9c675dd2a1cf10d34f3d
Fluffy9 <Fluffy9@users.noreply.github.com>
2022-03-17 21:14:03 -0400

    Change variable names, add comments

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
+// Port used for accessing gundb
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.env.GUNDB_PORT || process.argv[2] || 8765;
+// Import the Skynet GunDB Adapter
 var {zenbase} = require('zenbase')
 const server = require('http').createServer().listen(port);
 
-var gun = zenbase({
+var gundb = zenbase({
     web: server,
-    // the secret for skynet
-    secret: process.env["SECRET"] || "YOUR_SECRET_HERE", 
+    // SkyDB Secret used for portals to be able to access the same GunDB data
+    secret: process.env["SECRET"] || "SkyDB GunDB Production Secret", 
     // the skynet portal
     portal: process.env["PORTAL"] || "https://siasky.net", 
     // whether we want additional debug settings on
-    debug: process.env["DEBUG"] || true,
+    debug: process.env["DEBUG"] || false,
     // decreases the frequency that gun writes to the storage adapter. See issue here: https://github.com/Fluffy9/Zenbase/issues/1#issuecomment-823504402 
     until: process.env["UNTIL"] || 2*1000
 }) 
 
-
+console.log(`SECRET set to ${process.env["SECRET"]}, PORTAL set to ${process.env["PORTAL"]}, DEBUG set to ${process.env["DEBUG"]}, UNTIL set to ${process.env["UNTIL"]}`)
 console.log('Relay peer started on port ' + port + ' with /gun');
-module.exports = gun
+module.exports = gundb
